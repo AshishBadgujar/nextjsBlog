7aa4a5f3e9f6113e2c62a31c720dce37fd26c481
Ashish <ashishbadguajr124@gamil.com>
2020-11-01 21:14:06 +0530

    ui

--- components/footer.js
+++ components/footer.js
@@ -21,12 +21,12 @@ export default function Footer() {
             </div>
 
             <div className="footer">
-                <div className="container">
+                <div className="container mb-0">
                     <div className="row">
                         <div className="col-12">
                             <div className="copyright">
                                 <p>
-                                    Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i className="icon-heart text-danger" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank" >Colorlib</a>
+                                    Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i className="icon-heart text-danger" aria-hidden="true"></i> by <a target="_blank" >Ashish</a>
                                 </p>
                             </div>
                         </div>
